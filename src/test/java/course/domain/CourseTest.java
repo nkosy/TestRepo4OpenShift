25622985488fd4e33c9c1f2d582380b7039482bf
hashcode <test.com>
2015-04-23 12:33:55 +0200

    Added a factory Method for Creating Course

--- src/test/java/course/domain/CourseTest.java
+++ src/test/java/course/domain/CourseTest.java
@@ -1,8 +1,14 @@
 package course.domain;
 
+import course.conf.factory.CourseFactory;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by hashcode on 2015/04/13.
@@ -15,9 +21,40 @@ public class CourseTest {
     }
 
     @Test
-    public void testCourse() throws Exception {
-        Course course = new Course.Builder("ND4").name("NDP").offering(2015).build();
-        Assert.assertEquals("NDP",course.getName());
+    public void testCreateCourse() throws Exception {
+        List<Subject> lecturers = new ArrayList<Subject>();
+        Map<String,String> values = new HashMap<String,String>();
+
+        values.put("code","NDP2015");
+        values.put("name", "National Diploma");
+
+        Course course =CourseFactory
+                .createCourse(25, values, lecturers);
+
+        Assert.assertEquals("NDP2015",course.getCode());
+    }
+
+    @Test
+    public void testUpadteCourse() throws Exception {
+        List<Subject> lecturers = new ArrayList<Subject>();
+        Map<String,String> values = new HashMap<String,String>();
+
+        values.put("code","NDP2015");
+        values.put("name", "National Diploma");
+
+        Course course =CourseFactory
+                .createCourse(25, values, lecturers);
+
+        Course newcourse = new Course
+                .Builder(course.getCode())
+                .copy(course)
+                .offering(20).build();
+
+        Assert.assertEquals("NDP2015",newcourse.getCode());
+        Assert.assertEquals("National Diploma",newcourse.getName());
+        Assert.assertEquals(20,newcourse.getOffering());
+        Assert.assertEquals(25,course.getOffering());
+
 
     }
 }
